6456ba5f8b576659974a2a720ac9bab5abc06caf
iroskoshnyi <ivan.roskoshnyi@chromeriver.com>
2020-02-13 20:07:24 +0200

    add markerk

--- screens/events/EventMap.js
+++ screens/events/EventMap.js
@@ -1,11 +1,57 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
-import {StyleSheet} from 'react-native';
-import MapView from 'react-native-maps'
+import {StyleSheet, Text} from 'react-native';
+import MapView, {Marker} from 'react-native-maps'
+
+import * as Permissions from 'expo-permissions';
+import * as Location from 'expo-location'
 
 const EventMap = () => {
+  const [state, setState] = useState({
+    errorMessage: '',
+    permissionAsked: false,
+    location: null
+  });
+  useEffect(() => {
+    Permissions.askAsync(Permissions.LOCATION)
+      .then(({status}) => {
+        setState({
+          ...state,
+          permissionAsked: true
+        });
+        if (status !== 'granted') {
+          setState({
+            ...state,
+            errorMessage: 'Permission denied'
+          });
+        }
+        Location.getCurrentPositionAsync()
+          .then(location => {
+            setState({
+              ...state,
+              location
+            })
+          })
+      })
+  }, []);
+
+  if (state.errorMessage) {
+    return (
+      <Text>
+        {state.errorMessage}
+      </Text>
+    )
+  }
   return (
-    <MapView style={styles.map} />
+    <MapView
+      style={styles.map}
+      initialRegion={state.location && {...state.location.coords,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,}
+      }
+    >
+      {state.location && <Marker coordinate={state.location.coords} title='EventPlace' />}
+    </MapView>
   );
 };
 const styles = StyleSheet.create({
